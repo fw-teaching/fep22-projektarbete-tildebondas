bca6ef418494e4a386c5f8a3d6014f5fdde25a72
Robin Daniel Tildeman <Robin@tildeman.ax>
2022-11-07 16:11:15 +0200

    Java script för age check

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 /* Site-wide JS i den här filen (t.ex. huvudmenyn) */
 
+
+//the function that checks the age and if underage exits the site with a promt
+function ageCheck()
+ {
+    let ageCheck = confirm("I confirm am older than 18?");  
+
+    if(ageCheck){
+        const money = prompt ("how much do you want to put in?")
+        
+    }
+    else{
+        alert("Underage gambling is not allowed!")
+        window.close();
+    }
+
+
+
+    
+}
+
 console.log('main.js init'); // För att se att skriptet laddats in
