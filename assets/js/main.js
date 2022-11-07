a202b195862695f5fb67307783d0f20c3b60188c
F365 4 <elias.nybondas@gmail.com>
2022-11-07 15:38:22 +0200

    styling, js exempel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,30 @@
 /* Site-wide JS i den här filen (t.ex. huvudmenyn) */
 
 console.log('main.js init'); // För att se att skriptet laddats in
+
+function minFunktion() {
+    console.log("Funktionen funkar");
+}
+
+minFunktion();
+
+function buttonHandler() {
+    console.log("Du klickade här!");
+}
+
+function secondButton() {
+    const namn = prompt("Ge mig namn nam nam");
+    alert("Hello "+namn);
+}
+
+function thirdButton(){
+    let namn = document.querySelector("#namn").value;
+    document.getElementById("namn").value = "";
+    console.log(namn);
+
+    if (namn == "Elias") {
+        namn = `${namn} Nybondas`;
+    }
+
+    document.querySelector("#hello-output").innerText = `Morjens ${namn}!`;
+}
