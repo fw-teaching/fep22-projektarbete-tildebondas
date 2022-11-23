4caba3e64c3a4dafd8db7b270161e93971bc20f9
saile336 <elias.nybondas@gmail.com>
2022-11-23 15:51:17 +0200

    fråga fredde om menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,7 +2,7 @@ let colorValue = {R: 27, G: 87, B: 27}; //omvandla till array utan objekt så fu
 const sliderArr = [];
 document.querySelectorAll('.slider').forEach((elem) => sliderArr.push({color: elem.getAttribute('name'), value: elem.getAttribute('value')}));;
 function updateSlider(){
-    sliderArr.forEach((elem) => elem.value = colorValue.(elem.color); //omvandla till array utan objekt så funkar det (testat)
+    /*sliderArr.forEach((elem) => elem.value = colorValue.(elem.color); //omvandla till array utan objekt så funkar det (testat)*/
     console.log(sliderArr);
 }
 
