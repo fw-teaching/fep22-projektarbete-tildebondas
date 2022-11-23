0e4356552ae2d7355e9324322d4fa9c087d0f00a
saile336 <elias.nybondas@gmail.com>
2022-11-23 20:39:44 +0200

    duun på sliders

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,31 @@
-let colorValue = {R: 27, G: 87, B: 27}; //omvandla till array utan objekt så funkar det (testat)
+const colorValue = {R: 27, G: 87, B: 27};
 const sliderArr = [];
-document.querySelectorAll('.slider').forEach((elem) => sliderArr.push({color: elem.getAttribute('name'), value: elem.getAttribute('value')}));;
-function updateSlider(){
-    /*sliderArr.forEach((elem) => elem.value = colorValue.(elem.color); //omvandla till array utan objekt så funkar det (testat)*/
-    console.log(sliderArr);
-}
+const slider = document.querySelectorAll('.slider');
 
-updateSlider();
+slider.forEach((elem) => {
+    sliderArr.push({color: elem.getAttribute('color'), value: elem.getAttribute('value')})
+    //elem.setAttribute('value', colorValue[elem.color]);
+
+    elem.addEventListener('input', updateSlider);
+    console.log(elem.value);
+});;
+
+document.querySelector("#R").value=colorValue.R;
+document.querySelector("#G").value=colorValue.G;
+document.querySelector("#B").value=colorValue.B;
+
+
+function updateSlider(){
+    colorValue.R = document.querySelector("#R").value;
+    colorValue.G = document.querySelector("#G").value;
+    colorValue.B = document.querySelector("#B").value;
+    document.querySelector("div.basicBox label[for='R']").innerText = colorValue.R;
+    document.querySelector("div.basicBox label[for='G']").innerText = colorValue.G;
+    document.querySelector("div.basicBox label[for='B']").innerText = colorValue.B;
+
+    sliderArr.forEach((elem) => elem.value = colorValue[elem.color]);
+
+    localStorage.setItem('R', colorValue.R);
+    localStorage.setItem('G', colorValue.G);
+    localStorage.setItem('B', colorValue.B);
+}
